05a34d11fa74a3271a7180031c7b8c2264da09ba
NateNjuguna <ngrafix0faiv@gmail.com>
2017-01-18 16:35:01 +0300

    Bugfix

--- api_app/my_app.py
+++ api_app/my_app.py
@@ -1,6 +1,6 @@
 from custom_api import Resource
 import sys
-my_api = Resource(sys.argv[1], sys.argv[2], 8090, {'Content-Type':'application/json', 'charset':'utf-8'})
+my_api = Resource(sys.argv[1], sys.argv[2], sys.argv[3], {'Content-Type':'application/json', 'charset':'utf-8'})
 
 print 'A simple program to refresh a token at the public API found here -' + str(sys.argv[1]) + str(sys.argv[2])
 print 'Add it to the header and use it to fetch a list of users'
